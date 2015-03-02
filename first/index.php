0987f5553ec5a12dcdefbf8136d90c26b7c60fe5
Emir <emiratesbp@gmail.com>
2015-03-02 16:15:08 +0100

    First commit

---
+++ first/index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<link rel="stylesheet" href="css/jquery.mobile-1.4.5.css">
+
+		<script src="js/jquery-1.11.2.js"></script>
+		<script>
+		    $(document).on("mobileinit", function(evt) {
+		      // Change some settings
+		    	$.mobile.defaultPageTransition = "flip";
+		    	$.mobile.pageLoadErrorMessage = "Whoops!";
+		    	$.mobile.pageLoadErrorMessageTheme = "b";
+			});
+		</script>
+		<script src="js/jquery.mobile-1.4.5.js"></script>
+		<link rel="stylesheet" href="css/slicknav.css" />
+		<script src="js/jquery.slicknav.js"></script>
+		<script type="text/javascript" src="js/responsiveslides.js"></script>
+		<link rel="stylesheet" href="css/style-mobile.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script>
+			
+			$(function(){
+			   	$( "[data-role='header'], [data-role='footer']" ).toolbar();
+			});	    	
+		   	$(function(){
+				$('#menu').slicknav({
+					prependTo:'#navigation-menu'
+				});
+			});
+			$(function(){
+				$('#menu-contact').slicknav({
+					prependTo:'#navigation-menu-contact'
+				});
+			});
+			$(function(){
+				$('#menu-seo').slicknav({
+					prependTo:'#navigation-menu-seo'
+				});
+			});
+			$(function () {
+
+		      // Slideshow 4
+		      	$("#slider4").responsiveSlides({
+			        auto: true,
+			        pager: false,
+			        nav: true,
+			        speed: 500,
+			        namespace: "callbacks",
+			        before: function () {
+			          $('.events').append("<li>before event fired.</li>");
+			        },
+			        after: function () {
+			          $('.events').append("<li>after event fired.</li>");
+			        }
+		      	});
+
+		    });
+		    $(function () {
+
+		      // Slideshow web
+		      	$("#slider-web").responsiveSlides({
+			        auto: true,
+			        pager: false,
+			        nav: false,
+			        speed: 500,
+			        namespace: "callbacks",
+			        before: function () {
+			          $('.events').append("<li>before event fired.</li>");
+			        },
+			        after: function () {
+			          $('.events').append("<li>after event fired.</li>");
+			        }
+		      	});
+
+		    });
+		   
+			
+			
+    	</script>
+    	
+	</head>
+	<body>
+		
+		<?php 
+			include 'settings.php';
+			
+			
+		?>
+		<div id="header-image" data-role="header" data-theme="<?php echo $theme ?>" data-position="fixed">
+		    <h1><img src="img/logo.png" alt="LOGO"></h1>
+		</div>
+		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
+		    <h1>Mobile Site<a href="http://marketrivals.com" rel="external">  Clasic Site</a></h1>
+		</div>
+<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
+		  
+
+		  <div data-role="main" class="ui-content" data-theme="<?php echo $theme ?>">
+		  		<!-- Jssor Slider Begin -->
+			  	<div class="callbacks_container">
+				    <ul class="rslides" id="slider4">
+				      <li>
+				        <img src="img/slider-images/01.jpg" alt="">
+				        <!-- <p class="caption">This is a caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/02.jpg" alt="">
+				        <!-- <p class="caption">This is another caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/03.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/04.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/05.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/06.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/07.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/08.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/09.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/10.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/11.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				    </ul>
+			    </div>
+			    <!-- Jssor Slider End -->
+				<fieldset class="ui-grid-a" data-theme="<?php echo $theme ?>">
+					<!-- OVDJE SE UPISUJE BROJ TELEFONA KOJI TREBA POZVATI  -->
+					<div class="ui-block-a"><a class="ui-btn ui-icon-phone ui-btn-icon-left buttons-radius" href="tel:<?php echo $telephone ?>">Call US</a></div>
+					<!-- ovdje se upisuje grad i adresa iz koje  -->
+					<div class="ui-block-b" id="findUS">
+						<script>
+							var ua = navigator.userAgent;
+							if(/Android|webOS|Opera Mini/i.test(navigator.userAgent) ) {
+								console.log("Android uslo");
+								var prostordugme=document.getElementById('findUS');
+								var dugme= document.createElement('a');
+								dugme.setAttribute('class', "ui-btn ui-icon-location ui-btn-icon-left buttons-radius");
+								dugme.setAttribute('href',"geo:0,0?q=<?php echo $adresa; ?>,<?php echo $grad; ?>,<?php echo $skracenica; ?>");
+								dugme.innerHTML="Find US";
+								prostordugme.appendChild(dugme);
+							}
+							else if(/iPhone|iPad|iPod/i.test(navigator.userAgent)){
+								console.log("Iphone ");
+								var prostordugme=document.getElementById('findUS');
+								var dugme= document.createElement('a');
+								dugme.setAttribute('class', "ui-btn ui-icon-location ui-btn-icon-left buttons-radius");
+								dugme.setAttribute('href',"http://maps.google.com/?daddr=<?php echo $adresa; ?>,<?php echo $grad; ?>,<?php echo $skracenica; ?> ");
+								dugme.innerHTML="Find US";
+								prostordugme.appendChild(dugme);
+							}
+							else if (ua.indexOf("BlackBerry") >= 0) {
+
+									console.log("Blakberu je prosao ")
+									var prostordugme=document.getElementById('findUS');
+									var dugme= document.createElement('a');
+									dugme.setAttribute('class', "ui-btn ui-icon-location ui-btn-icon-left buttons-radius");
+									dugme.setAttribute('href',"javascript:blackberry.launch.newMap({'address':{'address1':'<?php echo $adresa; ?>,<?php echo $grad; ?>,<?php echo $skracenica; ?>'}});");
+									dugme.innerHTML="Find US";
+									prostordugme.appendChild(dugme);
+									}		
+							else {
+								console.log("nije nigdje uslo default ")
+								var prostordugme=document.getElementById('findUS');
+								var dugme= document.createElement('a');
+								dugme.setAttribute('class', "ui-btn ui-icon-location ui-btn-icon-left buttons-radius");
+								dugme.setAttribute('href',"geo:0,0?q=<?php echo $adresa; ?>,<?php echo $grad; ?>,<?php echo $skracenica; ?>");
+								dugme.innerHTML="Find US";
+								prostordugme.appendChild(dugme);
+							}
+
+							
+
+							// if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+							// // some code..
+							// }
+
+						</script>
+						
+						
+
+					</div>	   
+				</fieldset>
+				
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">About Us</a>
+					</div>
+				</div>
+				<div data-role="collapsible-set">	
+					<div data-role="collapsible" data-theme="<?php echo $theme ?>" data-content-theme="<?php echo $theme ?>" data-iconpos="right" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d">
+					   <h3>Food</h3>
+					   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Diner Menu</a>
+					   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#seopage" data-transition="<?php echo $transitionefect ?>">Kid's Menu</a>
+					   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Specials</a>
+					   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Cab's to go</a>
+					</div>					
+				</div>			
+					
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-calendar ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Upcoming Events</a>
+					</div>
+				</div>
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-edit ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Reservations</a>
+					</div>
+				</div>
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-shop ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Gift Certificates</a>
+					</div>
+				</div>
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-clock ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Office Hours</a>
+					</div>
+				</div>
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-mail ui-btn-icon-left buttons-radius" href="#contactform" data-transition="<?php echo $transitionefect ?>">Contact US</a></div>
+				</div>				   
+		  </div>
+		  <blockquote style="margin-right: 0px" dir="ltr">
+			<p dir="ltr" align="left">&nbsp;</p>
+			<p dir="ltr" align="left"><span style="font-size: 36px"><font color="#ffffff" face="Trebuchet MS, Arial, Helvetica, sans-serif">Cab's Bistro</font></span> <span style="font-size: 14px"><font color="#ffffff">is located on Main Street, in beautiful downtown Glen Ellyn, Illinois - just two blocks </font></span><span style="font-size: 14px"><font color="#ffffff">southwest from the Metra train station (Union Pacific/West line).</font></span></p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left">&nbsp;</p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left"><b><span style="font-family: 'trebuchet ms'; color: #993366; font-size: 16pt"><font color="#ffffff">Chef owned and operated</font></span></b></p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left"><span style="font-size: 14px"><font color="#ffffff">The restaurant is owned and operated by the husband / wife team of Chef Luis and </font></span><span style="font-size: 14px"><font color="#ffffff">Lisa Ayllon (General Manager).</font></span></p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left">&nbsp;</p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left"><b><span style="font-family: 'trebuchet ms'; color: #993366; font-size: 16pt"><font color="#ffffff">Great Food!</font></span></b></p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left"><span style="font-size: 14px"><font color="#ffffff"><span style="font-size: 10.5pt">Cab's eclectic menu is heavily influenced by Chef Luis Ayllon’s training in France. Along his affinity for Spanish, Asian, Cajun and Mexican cuisines; he also specializes in sauces, wine &amp; food matches, seafood, and game. </span></font></span></p>
+			<p class="MsoNormal">&nbsp;</p>
+			<p class="MsoNormal"><span style="font-size: 14px"><font color="#ffffff"><span style="font-size: 10.5pt">Some of the house favorites include the seafood combination, paella, steak frites,&nbsp;tuna tartar, beef bourguignon &amp; Cassoulet (in colder months); fried beer battered green beans and grilled&nbsp;vegetable fondue&nbsp;dishes.</span></font></span></p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left">&nbsp;</p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left"><b><span style="font-family: 'trebuchet ms'; color: #993366; font-size: 16pt"><font color="#ffffff">Special diet?</font></span></b></p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left"><span style="font-size: 14px"><font color="#ffffff">At Cab's we are happy to adjust to accommodate our customers dietary needs and desires. </font></span><span style="font-size: 14px"><font color="#ffffff">Gluten free diets, Eat right for your blood type diets, less fat, no starch extra vegetables, no sauce... </font></span><span style="font-size: 14px"><font color="#ffffff">Let us know what you want and we can usually do it!</font></span></p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left">&nbsp;</p>
+			<p style="margin: 0in 0in 0pt" dir="ltr" class="MsoNormal" align="left">&nbsp;</p>
+			</blockquote>
+		 
+		</div>
+<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+<!-- page 2 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+		<div data-role="page" id="page2" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" data-theme="<?php echo $theme ?>">
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu">
+						<ul id="menu">
+							<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">Home</a></li>
+							<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Seo</a></li>
+							<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Local SE0</a></li>
+							<li><a href="#contcatform" data-transition="<?php echo $transitionefect ?>">Contact us</a></li>
+						</ul>
+					</section>
+					<p>This is only a <b>PREVIEW</b> version of your Mobile Website.</p>
+					<p>Please contatact our official Sales Representative for <b>Full Functional Version.</b></p>
+						   
+				</div>
+			</div>	
+		</div>
+		
+<!-- page 2 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+<!-- contact us ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		
+		<div id="contactform" data-role="page" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" >
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu-contact">
+							<ul id="menu-contact">
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">Home</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Web Design</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Local SE0</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Web Analytics</a></li>
+							</ul>
+					</section>
+					<div>	
+						<form data-parsley-validate method="post" action="mail.php" data-ajax="false">
+							Name: <input type="text" id="naam" name="naam" value="" placeholder="Name" required/><br>
+							E-mail: <input type="email" id="mail" name="mail" value="" placeholder="E-mail" required/><br>
+							Phone Number: <input type="tel" id="telefoon" name="telefoon" value="" placeholder="Phone"/><br>
+							Subject: <input type="text" id="onderwerp" name="onderwerp" value="" placeholder="Subject" required/><br>
+							Message:<br>
+							<textarea style="height:100px;" id="bericht" name="bericht" placeholder="Enter your message here..." required></textarea> <br />
+							<input type="submit" value="Send" name="submit" class="btn" id="input-dugme">
+						</form>
+					</div>
+				</div>
+			</div>
+		
+		</div>
+<!--contact us///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+
+<!-- page seo ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div id="seopage" data-role="page" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" >
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu-seo">
+							<ul id="menu-seo">
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">Home</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Web Design</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Local SE0</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Web Analytics</a></li>
+							</ul>
+					</section>
+					<h2>This is a Mockup!</h2>
+				</div>
+			</div>
+		
+		</div>
+<!--page seo///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+
+<!--page succes///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+
+		<div id="succes" data-role="page" data-theme="<?php echo $theme ?>" data-dialog="true">
+		
+			<div data-role="main" class="ui-content" >	
+			 <p>The contact form was sent succesful!</p>
+			
+			</div>
+		
+		</div>
+<!--page succes ends here///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div id="error" data-role="page" data-dialog="true">
+		
+			<div data-role="main" class="ui-content" >	
+				<p>The contact form was sent because of an error!</p>
+			</div>
+		
+		</div>
+
+<!--erroor ends here////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		<!-- SLIDER ZA MOBILNI SAJT POCINJE OVDJEE -->
+		<!-- it works the same with all jquery version from 1.x to 2.x -->
+	
+		<script type="text/javascript" src="js/responsiveslides.js"></script>    
+	
+	   
+		<!-- Slajder se zavrsava -->
+	</body>
+</html>
